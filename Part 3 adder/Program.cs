b39516bc268ae173120e5f99d54b36916bddaf94
David Thomas <ThomDavi727@GoTVDSB.ca>
2020-09-30 10:27:28 -0400

    Add project files.

---
+++ Part 3 adder/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Part_3_adder
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int number1;
+            int number2;
+            int number3;
+
+            Console.WriteLine("Enter your first number: ");
+            number1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter your second number: ");
+            number2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter your third number: ");
+            number3 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"Your total is " + (number1 + number2 + number3));
+
+
+        }
+    }
+}
